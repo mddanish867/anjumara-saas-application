5732de2b044db92e96558ab3a29095ebcad8b9c8
mddanish867 <mddanish867@gmail.com>
2024-08-31 22:57:35 +0530

    added down option in sql and also added new file html2PDF

--- src/Pages/Database/Sql.tsx
+++ src/Pages/Database/Sql.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Breadcrumb from "@/BreadCrum/Breadcrum";
 import sql from "@/Data/sql.json";
 import Pagination from '../../Pagination/Pagination';
+import html2pdf from 'html2pdf.js';
 
 // Define the types for SQL data
 interface SQLQuestionAnswer {
@@ -45,62 +46,34 @@ function Sql() {
   const endIndex = startIndex + itemsPerPage;
   const paginatedData = Object.values(sqlData).flat().slice(startIndex, endIndex);
 
-  return (
-    <div className="max-w-screen-xl mx-auto px-4 pt-20 md:px-8">
-      {/* Breadcrumb */}
-      <Breadcrumb />
-      {/* Top Section: Title and Image */}
-      <div className="flex flex-col md:flex-row items-center">
-        <img
-          src="/sqlbaner.jpg"
-          alt="SQL Banner"
-          className="w-20 h-20 md:w-32 md:h-32 lg:w-40 lg:h-40 mb-4 md:mb-0 rounded-full shadow-md object-contain"
-        />
-        <div className="md:ml-4 text-center md:text-left">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-[#38bdf8] mb-2">
-            SQL Interview Preparation
-          </h2>
-          <p className="text-gray-600 text-sm md:text-base lg:text-lg">
-            Enhance your SQL skills with detailed knowledge of database design,
-            query optimization, and advanced SQL concepts. Get ready to impress
-            in your SQL interviews!
-          </p>
-        </div>
-      </div>
+  // Function to handle PDF download
+  const handleDownloadPDF = () => {
+    const hiddenElement = document.getElementById('all-content-to-download');
+    
+    // Temporarily display the hidden content
+    if (hiddenElement) hiddenElement.style.display = 'block';
+    
+    const element = document.getElementById('all-content-to-download');
+    html2pdf().from(element).set({
+      margin: 1,
+      filename: 'sql-interview-preparation.pdf',
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+    }).save().then(() => {
+      // Hide the content again after PDF generation
+      if (hiddenElement) hiddenElement.style.display = 'none';
+    });
+  };
 
-      {/* Search Input */}
-      <div className="mt-8 flex">
-        <input
-          type="text"
-          placeholder="Search interview questions..."
-          className="w-full px-4 py-2 border rounded-l-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#38bdf8]"
-        />
-        <button className="bg-[#38bdf8] text-white px-4 py-2 rounded-r-lg hover:bg-[#38bdf8] transition duration-300 ease-in-out">
-          Search
-        </button>
-      </div>
-
-      {/* SQL Interview Tips */}
-      <div className="mt-8">
+  // Function to render all questions
+  const renderAllQuestions = () => {
+    return Object.entries(sqlData).map(([level, questions], levelIndex) => (
+      <div key={levelIndex}>
         <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
-          SQL Interview Preparation Tips
-        </h3>
-        <p className="text-gray-600 text-sm md:text-base lg:text-lg mb-4">
-          SQL interviews often focus on your ability to understand and write
-          efficient queries. Be prepared to discuss database normalization,
-          indexing, query optimization, and SQL joins. Practice writing queries
-          that retrieve data from multiple tables and understand the
-          implications of different SQL operations on performance.
-        </p>
-      </div>
-
-      {/* List of Interview Questions and Answers */}
-      <div className="mt-8">
-        <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
-          Common SQL Interview Questions
+          {level} SQL Interview Questions
         </h3>
         <div className="w-full space-y-4">
-          {paginatedData.map((item, index) => (
+          {questions.map((item:SQLQuestionAnswer, index:number) => (
             <div key={index} className="bg-white p-6 rounded-lg">
               <h4 className="text-lg font-semibold text-gray-800 mb-2">
                 {item.question}
@@ -109,6 +82,81 @@ function Sql() {
             </div>
           ))}
         </div>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="max-w-screen-xl mx-auto px-4 pt-20 md:px-8">
+      {/* Breadcrumb */}
+      <Breadcrumb />
+
+      {/* Download Button */}
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={handleDownloadPDF}
+          className="bg-[#38bdf8] text-white px-4 py-2 rounded-lg hover:bg-[#38bdf8] transition duration-300 ease-in-out"
+        >
+          Download PDF
+        </button>
+      </div>
+
+      {/* Content to be downloaded */}
+      <div id="content-to-download">
+        {/* Top Section: Title and Image */}
+        <div className="flex flex-col md:flex-row items-center">
+          <img
+            src="/sqlbaner.jpg"
+            alt="SQL Banner"
+            className="w-20 h-20 md:w-32 md:h-32 lg:w-40 lg:h-40 mb-4 md:mb-0 rounded-full shadow-md object-contain"
+          />
+          <div className="md:ml-4 text-center md:text-left">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-[#38bdf8] mb-2">
+              SQL Interview Preparation
+            </h2>
+            <p className="text-gray-600 text-sm md:text-base lg:text-lg">
+              Enhance your SQL skills with detailed knowledge of database design,
+              query optimization, and advanced SQL concepts. Get ready to impress
+              in your SQL interviews!
+            </p>
+          </div>
+        </div>
+
+        {/* SQL Interview Tips */}
+        <div className="mt-8">
+          <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
+            SQL Interview Preparation Tips
+          </h3>
+          <p className="text-gray-600 text-sm md:text-base lg:text-lg mb-4">
+            SQL interviews often focus on your ability to understand and write
+            efficient queries. Be prepared to discuss database normalization,
+            indexing, query optimization, and SQL joins. Practice writing queries
+            that retrieve data from multiple tables and understand the
+            implications of different SQL operations on performance.
+          </p>
+        </div>
+
+        {/* List of Interview Questions and Answers */}
+        <div className="mt-8">
+          <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
+            Common SQL Interview Questions
+          </h3>
+          <div className="w-full space-y-4">
+            {paginatedData.map((item, index) => (
+              <div key={index} className="bg-white p-6 rounded-lg">
+                <h4 className="text-lg font-semibold text-gray-800 mb-2">
+                  {item.question}
+                </h4>
+                <TextBlock text={item.answer} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Hidden content for PDF download */}
+      <div id="all-content-to-download" style={{ display: 'none' }}>
+        {renderAllQuestions()}
       </div>
 
       {/* Pagination */}
