3eca610ab82638e0c06a353fd50e85c0ea1e4d24
mddanish867 <mddanish867@gmail.com>
2024-09-03 13:15:47 +0530

    modified the openaiService

--- src/API/openaiService.ts
+++ src/API/openaiService.ts
@@ -1,6 +1,6 @@
 
-const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
-const apiUrl = import.meta.env.VITE_OPEN_API_URL;
+const apiKey: string = process.env.VITE_OPENAI_API_KEY ?? '';
+const apiUrl: string = process.env.VITE_OPEN_API_URL ?? '';
 
 
 
