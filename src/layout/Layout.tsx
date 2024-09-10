95d0a77656e89260e4b9f06796b4f3aea17ea4ca
Md Danish Akhtar <mddanish867@gmail.com>
2024-09-10 19:12:13 +0530

    Update Layout.tsx
    updated the layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col w-full min-h-screen">
       <Navbar />
-      <div className="-mt-14 mx-auto flex-1">
+      <div className="-mt-14">
         <Outlet />  {/* Use Outlet to render the matched child routes */}
       </div>
       <Footer />
