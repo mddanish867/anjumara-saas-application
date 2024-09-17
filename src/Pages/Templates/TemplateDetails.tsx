83568be4beb157ea7b19e85b5019ccd21aab58db
mddanish867 <mddanish867@gmail.com>
2024-09-17 20:33:25 +0530

    modified the template details

--- src/Pages/Templates/TemplateDetails.tsx
+++ src/Pages/Templates/TemplateDetails.tsx
@@ -1,7 +1,5 @@
-// ComponentDetails.jsx
-
 import { useState } from 'react';
-import { Home, ChevronRight, CodeXml, Eclipse } from 'lucide-react';
+import { CodeXml, Eclipse } from 'lucide-react';
 
 const TemplateDetails = () => {
   // Set the first image as the default selected image
@@ -25,38 +23,24 @@ const TemplateDetails = () => {
 
   return (
     <div className="p-6 mt-20">
-      {/* Breadcrumb */}
-      <nav className="text-gray-700 mb-4 flex items-center">
-        <Home className="w-4 h-4 mr-1" />
-        <a href="/" className="hover:underline">
-          Home
-        </a>
-        <ChevronRight className="w-4 h-4 mx-1" />
-        <a href="/components" className="hover:underline">
-          Components
-        </a>
-        <ChevronRight className="w-4 h-4 mx-1" />
-        <span className="font-semibold">Component Name</span>
-      </nav>
-
       {/* Image Section */}
-      <div className="flex flex-col md:flex-row mb-8">
+      <div className="flex flex-col-reverse md:flex-row mb-8">
         {/* Thumbnails */}
-        <div className="md:w-1/4 flex md:flex-col space-x-2 md:space-x-0 md:space-y-2 overflow-x-auto md:overflow-visible">
+        <div className="md:w-1/4 flex md:flex-col md:space-y-2 space-x-2 md:space-x-0 overflow-x-auto md:overflow-visible md:ml-6">
           {images.map((image) => (
             <img
               key={image.src}
               src={image.thumb}
               alt={image.alt}
-              className={`w-24 h-24 object-cover cursor-pointer border border-gray-200 hover:border-violet-500 ${
-                selectedImage === image.src ? 'border-violet-500' : ''
+              className={`w-24 h-24 object-cover cursor-pointer border border-gray-200 hover:border-[#38bdf8] ${
+                selectedImage === image.src ? 'border-[#38bdf8]' : ''
               }`}
               onClick={() => setSelectedImage(image.src)}
             />
           ))}
         </div>
         {/* Large Image */}
-        <div className="md:w-3/4 mt-4 md:mt-0 md:pl-4">
+        <div className="md:w-3/4 mb-4 md:mb-0 md:pl-4">
           <img
             src={selectedImage}
             alt="Selected"
@@ -80,7 +64,7 @@ const TemplateDetails = () => {
           const Icon = tech.icon;
           return (
             <li key={tech.name} className="flex items-center space-x-2">
-              <Icon className="w-6 h-6 text-violet-500" />
+              <Icon className="w-6 h-6 text-[#38bdf8]" />
               <span>{tech.name}</span>
             </li>
           );
@@ -106,7 +90,7 @@ const TemplateDetails = () => {
       <h2 className="text-2xl font-semibold mb-2">Documentation</h2>
       <p className="text-gray-700 mb-6">
         Refer to our{' '}
-        <a href="/docs/component-name" className="text-violet-500 underline">
+        <a href="/docs/component-name" className="text-[#38bdf8] underline">
           comprehensive documentation
         </a>{' '}
         for setup instructions, customization guides, and troubleshooting tips.
@@ -116,7 +100,7 @@ const TemplateDetails = () => {
       <h2 className="text-2xl font-semibold mb-2">API Documentation</h2>
       <p className="text-gray-700">
         Explore the{' '}
-        <a href="/api/component-name" className="text-violet-500 underline">
+        <a href="/api/component-name" className="text-[#38bdf8] underline">
           API documentation
         </a>{' '}
         to learn about available endpoints, parameters, and example requests and
